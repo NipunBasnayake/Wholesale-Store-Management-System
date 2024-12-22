b6b668435f6fecc38eaa5e7bb29c7615f937cc0c
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-12-23 00:51:04 +0530

    Place Order UI

--- ThogaKade/src/thogakade/ThogaKade.java
+++ ThogaKade/src/thogakade/ThogaKade.java
@@ -1,9 +1,11 @@
 package thogakade;
 
+import GUI.PalceOrderWindow;
+
 public class ThogaKade {
 
     public static void main(String[] args) {
-        
+        new PalceOrderWindow().setVisible(true);
     }
     
 }
